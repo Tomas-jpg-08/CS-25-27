6a4c011f08985f9069c832c14ceafce6ad3d886d
Tomas-jpg-08 <159006573+Tomas-jpg-08@users.noreply.github.com>
2025-10-28 12:06:26 +0300

    Create MultiplyArrays.java

---
+++ src/U1P1/MultiplyArrays.java
@@ -0,0 +1,39 @@
+package U1P1;
+
+public class MultiplyArrays {
+    public static void main(String[] args) {
+
+        int firstArray[] = new int[5];
+        int secondArray[] = new int[5];
+        int resultArray[] = new int[5];
+
+        //create
+        //multiply first and second array
+
+        firstArray[0] = 7;
+        firstArray[1] = 8;
+        firstArray[2] = 3;
+        firstArray[3] = 4;
+        firstArray[4] = 3;
+
+        secondArray[0] = 30;
+        secondArray[1] = 46;
+        secondArray[2] = 28;
+        secondArray[3] = 43;
+        secondArray[4] = 25;
+
+        for(int i=0; i<firstArray.length; i++) {
+            resultArray[i] = firstArray[i] * secondArray[i];
+           // System.out.println(resultArray[i]);
+        }
+
+        //sum everything
+        int sumArray = 0;
+        for(int i=0; i<resultArray.length; i++) {
+            sumArray = sumArray + resultArray[i];
+            System.out.println(sumArray);
+        }
+
+
+    }
+}
