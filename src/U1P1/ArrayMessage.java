7e924155c471a9cd11ab3e0e3fdfb737e352bd4b
Tomas-jpg-08 <159006573+Tomas-jpg-08@users.noreply.github.com>
2025-11-04 22:38:22 +0300

    asdkf
    as;df

--- src/U1P1/ArrayMessage.java
+++ src/U1P1/ArrayMessage.java
@@ -13,22 +13,41 @@ public class ArrayMessage {
 
         int rows = rawSignal.length;
         int cols = rawSignal[0].length;
-        int[][] transpose = new int[rows][cols];
+        int[][] transposed = new int[rows][cols];
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                transpose [j][i] = rawSignal[i][j];
+                transposed [j][i] = rawSignal[i][j];
             }
         }
 
-
-       
         // vertically reflect every even indexed column
+
+        for (int i = 0; i < transposed.length; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < transposed[i].length / 2; j++) {
+                    int tmp = transposed[i][j];
+                    transposed[i][j] = transposed[i] [transposed[i].length - 1 - j];
+                    transposed[i] [transposed[i].length - 1 - j] = tmp;
+                }
+            }
+
+        }
+
         // column 0 must be vertically reflected, etc.
         // column 1 must remain the same
 
         // Calculate the average of the 4 integers in each row.
+        for (int i = 0; i < transposed.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < transposed[i].length; j++) {
+                sum += transposed[i][j];
+            }
+            int avg = Math.round(sum / (float) transposed[i].length);
+            System.out.print((char) avg);
+        }
 
         // find the ASCII value of each result and print it to the console
+        System.out.println();
 
 
 
